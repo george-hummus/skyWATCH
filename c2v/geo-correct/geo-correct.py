f0ee8579cfb1ba13f276f96ecb9bcbe78fd4cd06
george-hummus <ghume1@sheffield.ac.uk>
2022-05-05 15:26:31 +0100

    tried geo-correct using cv.findHomography() - bad (updated links too)

--- c2v/geo-correct/geo-correct.py
+++ c2v/geo-correct/geo-correct.py
@@ -1,6 +1,3 @@
-#### NOT FINISHED ####
-exit()
-
 ### IMPORTS ###
 import cv2 as cv
 import numpy as np
@@ -9,13 +6,14 @@ import os
 
 #command line arguments
 parser = argparse.ArgumentParser(description = """
-Takes collection of coordinates of stars in an all-sky image and sky-map, and uses these to calculate a transformation matrix for the all-sky image. This can then be applied to all all-sky images so constellation overlay can be applied to it.
+Takes collection of coordinates of stars in an all-sky image and sky-map, and uses these to calculate a transformation matrix for the sky-map image. This can then be applied to all sky-map images so constellation overlay can be applied to the all-sky image.
 Author: George Hume
 2022
 """)
 #adding arguments to praser object
 parser.add_argument('coords' , type = str, help = 'Path to the CSV file containing the coordinates of corresponding stars in the all-sky image and sky-map.')
-parser.add_argument('--ASimage' , type = str, help = 'Path to the all-sky image file, if you want transformation to be applied to it at the end. By default this this will not occur', default = "None")
+parser.add_argument('--SMimage' , type = str, help = 'Path to the sky-map image file, if you want transformation to be applied to it at the end. By default this this will not occur.', default = "None")
+args = parser.parse_args()
 
 #load in a csv file with x,y points from all-sky image and x',y' points from the skymap for target stars
 coords=np.loadtxt(args.coords,delimiter=",",dtype="int",skiprows=1)
@@ -31,11 +29,19 @@ x1 = x1[unmatch]
 y1 = y1[unmatch]
 
 #do polynomial least fit to find warping coefficents of the X and Y transformation matrices
+#perspective transformation
+pts1 = np.float32(np.vstack((x,y)).T.reshape(-1,1,2))
+pts2 = np.float32(np.vstack((x1,y1)).T.reshape(-1,1,2))
+M,mask = cv.findHomography(pts1,pts2) #matrix will map sky-map onto all-sky
 
 #need to save the transformation matrix so it can be used again.
+np.save('transfrom-mat',M)
+
 
 #option to apply transformation matrix on each point of the all-sky image to correct it
-if args.ASimage != "None":
-
+if args.SMimage != "None":
+    smim = cv.imread(args.SMimage)
+    dst =  cv.warpPerspective(smim, M, (6000, 6000))
+    cv.imwrite(f'{args.SMimage[0:-4]}_dst.png',dst)
 else:
     exit()
